a7036a21ebc6bcef0e21f8802102fc40bf8fc796
kyorhane <63658901+kyorhane@users.noreply.github.com>
2022-03-19 18:10:59 +0630

    remove commerce files from main

--- auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Bid, Category, Listing, User, Comment
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Category)
-admin.site.register(Comment)
